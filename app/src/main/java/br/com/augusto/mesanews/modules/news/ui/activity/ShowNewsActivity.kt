397e9fdb807ac78a7edcd0c18539d2664807a869
Augusto Flávio Mendonça <augusto.flaviom@gmail.com>
2020-11-25 14:28:03 -0300

    Permitir compartilhar a noticia na tela de visualizacao

--- app/src/main/java/br/com/augusto/mesanews/modules/news/ui/activity/ShowNewsActivity.kt
+++ app/src/main/java/br/com/augusto/mesanews/modules/news/ui/activity/ShowNewsActivity.kt
@@ -1,6 +1,9 @@
 package br.com.augusto.mesanews.modules.news.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.app.ShareCompat
 import br.com.augusto.mesanews.R
 import br.com.augusto.mesanews.app.ui.WebViewProgressClient
 import br.com.augusto.mesanews.app.ui.activity.ToolbarActivity
@@ -35,4 +38,30 @@ class ShowNewsActivity: ToolbarActivity() {
         web_view.loadUrl(news!!.url)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val sharedFilter = menu?.add(1, MENU_SHARED, Menu.NONE, "Shared")
+        sharedFilter!!.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        sharedFilter.setIcon(R.drawable.ic_baseline_share_24)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    companion object {
+        private const val MENU_SHARED = 1
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == MENU_SHARED) {
+            sharedView(news!!)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun sharedView(news: News) {
+        ShareCompat.IntentBuilder.from(this)
+            .setType("text/plain")
+            .setChooserTitle(news.title)
+            .setText(news.url)
+            .setSubject(news.content)
+            .startChooser()
+    }
 }
