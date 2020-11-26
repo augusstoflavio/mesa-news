060745fcf7cf51e1c85e9ffc73f83de6dc3beefd
Augusto Flávio Mendonça <augusto.flaviom@gmail.com>
2020-11-25 21:20:35 -0300

    Adiciona na home um botao para visualizar os favoritos

--- app/src/main/java/br/com/augusto/mesanews/modules/news/ui/activity/NewsActivity.kt
+++ app/src/main/java/br/com/augusto/mesanews/modules/news/ui/activity/NewsActivity.kt
@@ -2,23 +2,25 @@ package br.com.augusto.mesanews.modules.news.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.augusto.mesanews.R
 import br.com.augusto.mesanews.app.data.Result
 import br.com.augusto.mesanews.app.helper.toast
-import br.com.augusto.mesanews.app.ui.activity.ToolbarActivity
 import br.com.augusto.mesanews.app.ui.adapter.OnClickItemAdapterListener
 import br.com.augusto.mesanews.modules.news.data.News
 import br.com.augusto.mesanews.modules.news.ui.adapter.HighlightsAdapter
 import br.com.augusto.mesanews.modules.news.ui.adapter.NewsAdapter
 import br.com.augusto.mesanews.modules.news.ui.viewModel.NewsViewModel
 import kotlinx.android.synthetic.main.activity_news.*
+import kotlinx.android.synthetic.main.app_bar.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
-class NewsActivity : ToolbarActivity() {
+class NewsActivity : AppCompatActivity() {
 
     private lateinit var adapterNews: NewsAdapter
     val viewModel: NewsViewModel by viewModel()
@@ -26,6 +28,7 @@ class NewsActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+        setSupportActionBar(toolbar_app)
         setTitle("News")
 
         adapterNews = NewsAdapter(
@@ -92,5 +95,17 @@ class NewsActivity : ToolbarActivity() {
         val intent = Intent(applicationContext, ShowNewsActivity::class.java)
         intent.putExtra("news", news)
         startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val favoriteMenu = menu?.add(1, MENU_FAVORITE, Menu.NONE, "Favorite")
+        favoriteMenu?.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        favoriteMenu?.setIcon(R.drawable.ic_heart)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    companion object {
+        private const val MENU_FAVORITE = 1
     }
 }
